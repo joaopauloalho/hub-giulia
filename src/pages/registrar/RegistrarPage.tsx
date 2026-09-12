22b1881c56d04f2a0695c30b1df75738dc024d9e
joaopauloalho <62901157+joaopauloalho@users.noreply.github.com>
2026-09-12 12:25:53 -0300

    feat: route existing attendances to edit mode

--- src/pages/registrar/RegistrarPage.tsx
+++ src/pages/registrar/RegistrarPage.tsx
@@ -1,10 +1,12 @@
 import { useSearchParams } from 'react-router-dom';
 import { TreatmentRegistrarPageV2 } from './TreatmentRegistrarPageV2';
 import { ReturnRegistrarPage } from './ReturnRegistrarPage';
+import { EditAttendancePage } from './EditAttendancePage';
 import './registrar-workspace.css';
 
 export function RegistrarPage() {
   const [searchParams] = useSearchParams();
+  const isEdit = Boolean(searchParams.get('edit'));
   const isReturn = Boolean(searchParams.get('return_of'));
-  return <div className="registrar-route">{isReturn ? <ReturnRegistrarPage /> : <TreatmentRegistrarPageV2 />}</div>;
+  return <div className="registrar-route">{isEdit ? <EditAttendancePage /> : isReturn ? <ReturnRegistrarPage /> : <TreatmentRegistrarPageV2 />}</div>;
 }
